384e52b11216db92f0e55782df0ec1b62cf69958
Douglas Marquardt <16510416+marqdouj@users.noreply.github.com>
2025-06-07 10:53:39 -0600

    added Geolocation package

--- src/BlazorSandbox/BlazorSandbox/BlazorSandbox.Client/Program.cs
+++ src/BlazorSandbox/BlazorSandbox/BlazorSandbox.Client/Program.cs
@@ -1,7 +1,11 @@
+using Marqdouj.Html.Geolocation;
+using Marqdouj.Html.Geolocation.Models;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddFluentUIComponents();
+
+builder.Services.AddScoped<IGeolocationService, GeolocationService>();
 
 await builder.Build().RunAsync();
--- src/BlazorSandbox/BlazorSandbox/BlazorSandbox/Program.cs
+++ src/BlazorSandbox/BlazorSandbox/BlazorSandbox/Program.cs
@@ -1,5 +1,6 @@
-using BlazorSandbox.Client.Pages;
 using BlazorSandbox.Components;
+using Marqdouj.Html.Geolocation;
+using Marqdouj.Html.Geolocation.Models;
 using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 builder.Services.AddFluentUIComponents();
+
+builder.Services.AddScoped<IGeolocationService, GeolocationService>();
 
 var app = builder.Build();
 
